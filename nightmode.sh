9617e1112f71ff2232b1be33ca96bedeef687713
Kunal Chand <kunalchand234@gmail.com>
2023-11-29 08:24:54 -0500

    Update nightmode.sh

--- nightmode.sh
+++ nightmode.sh
@@ -3,7 +3,9 @@
 # Set the script to exit immediately if any command fails
 set -e
 
+# Resest any light changes to default temperature 6500K
 redshift -x
+# Set the light to night temperature 3800K
 redshift -O 3800
 
 # Get the PID of the terminal

a889ec4c6af46bcf9c79ab3b292e45828d16ed16
Kunal Chand <kunalchand234@gmail.com>
2023-12-03 18:03:31 -0500

    open vscode script

--- browser.sh
+++ browser.sh
@@ -19,31 +19,32 @@ COMMAND_TO_WEBSITE["portfolio"]="https://kunalchand.github.io/portfolio/"
 COMMAND_TO_WEBSITE["linkedin"]="https://www.linkedin.com/in/kunal-chand/"
 COMMAND_TO_WEBSITE["github"]="https://github.com/kunalchand"
 COMMAND_TO_WEBSITE["leetcode"]="https://leetcode.com/kunalchand234/"
+COMMAND_TO_WEBSITE["music"]="https://music.youtube.com/"
 
 # Get the command that was passed as a parameter to the script
 COMMAND=$1
 
 # Check if the command is "list"
 if [[ "$COMMAND" == "list" ]]; then
- # If the command is "list", print the list of commands and their respective website links
- for COMMAND in "${!COMMAND_TO_WEBSITE[@]}"; do
-  # ANSI escape codes for text formatting
-  BOLD=$(tput bold)
-  RESET=$(tput sgr0)
-  COLOR_COMMAND=$(tput setaf 6)  # Set color to cyan, you can change this to another color
-  echo "${BOLD}${COLOR_COMMAND}$COMMAND${RESET}: ${COMMAND_TO_WEBSITE[$COMMAND]}"
- done
+    # If the command is "list", print the list of commands and their respective website links
+    for COMMAND in "${!COMMAND_TO_WEBSITE[@]}"; do
+        # ANSI escape codes for text formatting
+        BOLD=$(tput bold)
+        RESET=$(tput sgr0)
+        COLOR_COMMAND=$(tput setaf 6)  # Set color to cyan, you can change this to another color
+        echo "${BOLD}${COLOR_COMMAND}$COMMAND${RESET}: ${COMMAND_TO_WEBSITE[$COMMAND]}"
+    done
 else
- # If the command is not "list", use the associative array to determine the website
- WEBSITE=${COMMAND_TO_WEBSITE[$COMMAND]}
+    # If the command is not "list", use the associative array to determine the website
+    WEBSITE=${COMMAND_TO_WEBSITE[$COMMAND]}
 
- # Launch browser in the background
- setsid google-chrome $WEBSITE > /dev/null 2>&1 &
+    # Launch browser in the background
+    setsid google-chrome $WEBSITE > /dev/null 2>&1 &
 
- # Get the PID of the terminal
- TERMINAL_PID=$(ps -o ppid= -p $$)
+    # Get the PID of the terminal
+    TERMINAL_PID=$(ps -o ppid= -p $$)
 
- # Close the terminal
- kill -9 "$TERMINAL_PID"
+    # Close the terminal
+    kill -9 "$TERMINAL_PID"
 fi
 

72839d9f809e1f5b515196fd44a6a17ebaf96e8b
Kunal Chand <kunalchand234@gmail.com>
2023-12-03 15:47:08 -0500

    added couple more commands

--- browser.sh
+++ browser.sh
@@ -11,6 +11,10 @@ COMMAND_TO_WEBSITE["google"]="https://www.google.com/"
 COMMAND_TO_WEBSITE["roadmap"]="https://neetcode.io/roadmap"
 COMMAND_TO_WEBSITE["leetcodenotes"]="https://docs.google.com/spreadsheets/d/14xdtVdBHRXI2thUCdcIiq0E-C1gLPzHMGUZl31hw6gc/edit#gid=1336779853"
 COMMAND_TO_WEBSITE["javanotes"]="https://docs.google.com/document/d/1JSchYJH05EUrNvdm1mI50um1R79Pi9bkoH9SmEWy_b8/edit#heading=h.pvt1ihed3m2z"
+COMMAND_TO_WEBSITE["whatsapp"]="https://web.whatsapp.com/"
+COMMAND_TO_WEBSITE["youtube"]="https://www.youtube.com/channel/UCOnFr4jtlNNwHk5ERjsJ54Q/playlists?view=1&sort=lad"
+COMMAND_TO_WEBSITE["gmail"]="https://mail.google.com/mail/u/0/#inbox"
+COMMAND_TO_WEBSITE["tracker"]="https://docs.google.com/spreadsheets/d/1G4kL5vfjjpOjM6wQHF3YPOZsD1FGpF0v9oAscTS7Ky4/edit#gid=0"
 
 # Get the command that was passed as a parameter to the script
 COMMAND=$1
@@ -19,7 +23,11 @@ COMMAND=$1
 if [[ "$COMMAND" == "list" ]]; then
  # If the command is "list", print the list of commands and their respective website links
  for COMMAND in "${!COMMAND_TO_WEBSITE[@]}"; do
-   echo "$COMMAND: ${COMMAND_TO_WEBSITE[$COMMAND]}"
+  # ANSI escape codes for text formatting
+  BOLD=$(tput bold)
+  RESET=$(tput sgr0)
+  COLOR_COMMAND=$(tput setaf 6)  # Set color to cyan, you can change this to another color
+  echo "${BOLD}${COLOR_COMMAND}$COMMAND${RESET}: ${COMMAND_TO_WEBSITE[$COMMAND]}"
  done
 else
  # If the command is not "list", use the associative array to determine the website

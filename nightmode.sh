dde13c0d22de1b3d9cd8809702c87ef3ec4853ac
Kunal Chand <kunalchand234@gmail.com>
2023-12-04 02:35:40 -0500

    nightmode.sh temp parameter

--- nightmode.sh
+++ nightmode.sh
@@ -3,13 +3,24 @@
 # Set the script to exit immediately if any command fails
 set -e
 
-# Resest any light changes to default temperature 6500K
+# Reset any light changes to default temperature 6500K
 redshift -x
-# Set the light to night temperature 3800K
-redshift -O 3800
+
+# Default Night Temperature = 3500K
+DEFAULT_TEMPERATURE=3500
+
+# Check if a parameter was passed and if it's a numeric value within the range 2000 and 4000
+if [ "$#" -eq 1 ] && [[ $1 =~ ^[0-9]+$ ]] && (( $1 >= 2000 && $1 <= 4000 )); then
+   # Set the light to the passed temperature
+   redshift -O $1
+else
+   # Set the light to default night temperature
+   redshift -O $DEFAULT_TEMPERATURE
+fi
 
 # Get the PID of the terminal
 TERMINAL_PID=$(ps -o ppid= -p $$)
 
 # Close the terminal
 kill -9 "$TERMINAL_PID"
+

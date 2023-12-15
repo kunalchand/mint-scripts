847132e4d1e826af9213e541f152c3dd32204365
Kunal Chand <kunalchand234@gmail.com>
2023-12-14 23:49:21 -0500

    practice.sh (browser + vscode)

---
+++ practice.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set the script to exit immediately if any command fails
+set -e
+
+ROADMAP="https://neetcode.io/roadmap"
+LEETCODENOTES="https://docs.google.com/spreadsheets/d/14xdtVdBHRXI2thUCdcIiq0E-C1gLPzHMGUZl31hw6gc/edit#gid=1336779853"
+JAVANOTES="https://docs.google.com/document/d/1JSchYJH05EUrNvdm1mI50um1R79Pi9bkoH9SmEWy_b8/edit#heading=h.pvt1ihed3m2z"
+
+# Launch browser in the background
+setsid google-chrome $ROADMAP > /dev/null 2>&1 &
+setsid google-chrome $LEETCODENOTES > /dev/null 2>&1 &
+setsid google-chrome $JAVANOTES > /dev/null 2>&1 &
+
+cd /home/kunalchand/Desktop/Projects/LeetCode
+code .
+
+# Get the PID of the terminal
+TERMINAL_PID=$(ps -o ppid= -p $$)
+
+# Close the terminal
+kill -9 "$TERMINAL_PID"

c2a91eee0b7ed74d5793cdf1091c5b83c6702db9
Kunal Chand <kunalchand234@gmail.com>
2023-12-15 19:14:02 -0500

    default folder open through vscode command

--- vscode.sh
+++ vscode.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd "/home/kunalchand/Desktop/Projects"
+
 # Open VSCode in a new session which detaches it fromt the terminal and runs independently
-setsid code "$@" > /dev/null 2>&1 &
+setsid code . "$@" > /dev/null 2>&1 &
 
 # Get the PID of the terminal
 TERMINAL_PID=$(ps -o ppid= -p $$)

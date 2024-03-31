5492ac7d6ea8fc79184f443f450b134f64700082
Kunal Chand <kunalchand234@gmail.com>
2024-03-31 06:23:05 -0400

    new imports on a new python file

--- p.sh
+++ p.sh
@@ -29,15 +29,17 @@ touch "$new_file_name"
 
 # Add specific content to the new file
 cat <<EOF >> "$new_file_name"
+import bisect
 import copy
 import heapq
 import math
 import random
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
 from collections import Counter, defaultdict, deque
 from dataclasses import dataclass
 from functools import cmp_to_key, reduce
 from itertools import zip_longest
-from math import factorial, sqrt
+from math import ceil, factorial, floor, sqrt
 from typing import Dict, List, Optional, Set, Tuple, Union
 EOF
 

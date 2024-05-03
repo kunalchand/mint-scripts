e9c913979c128043570a73b653a650cf8ceceac7
Kunal Chand <kunalchand234@gmail.com>
2024-05-03 13:58:03 -0400

    alias along with links in list command

--- browser.sh
+++ browser.sh
@@ -15,6 +15,22 @@ do
    COMMAND_TO_WEBSITE["$command"]="$url"
 done < "$SCRIPT_DIR/commands.txt"
 
+# Function to extract aliases from a given file
+extract_aliases() {
+    # Check if aliases file exists and is readable
+    if [ -r "$1" ]; then
+        # Use grep and sed to extract aliases and format them into a single line
+        grep -E "^alias [a-zA-Z0-9_]+='.*'" "$1" | sed -nE "s/^alias ([a-zA-Z0-9_]+)='.*'/\1/p" | tr '\n' ' '
+        echo "" # Add a new line for better formatting
+    else
+        echo "Aliases file '$1' not found or not readable."
+        exit 1
+    fi
+}
+
+# Extract aliases from the specified file
+aliases=$(extract_aliases "/home/kunalchand/Documents/mint-scripts/bashrc_alias.txt")
+
 if [ -z "$1" ]; then
     # If no parameter is passed, open Google Chrome
     setsid google-chrome > /dev/null 2>&1 &
@@ -38,6 +54,11 @@ else
             COLOR_COMMAND=$(tput setaf 6)  # Set color to cyan, you can change this to another color
             echo "${BOLD}${COLOR_COMMAND}$COMMAND${RESET}: ${COMMAND_TO_WEBSITE[$COMMAND]}"
         done
+
+        # List aliases
+        echo "--------------------------------------------------------------------------------"
+        echo "$(tput setaf 229)$(tput bold)Aliases:$(tput sgr0)"
+        echo -e "$(tput setaf 211)$aliases$(tput sgr0)"
     else
         # If the command is not "list", use the associative array to determine the website
         WEBSITE=${COMMAND_TO_WEBSITE[$COMMAND]}

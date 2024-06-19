11b5d4326b50ade5975fd6e8682e388a7b3afa04
Kunal Chand <kunalchand234@gmail.com>
2024-06-19 15:11:22 -0400

    update import statements in defualt python file

--- p.sh
+++ p.sh
@@ -37,10 +37,10 @@ import random
 from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right
 from collections import Counter, defaultdict, deque
 from dataclasses import dataclass
-from functools import cmp_to_key, reduce
-from itertools import zip_longest
-from math import ceil, factorial, floor, sqrt
-from typing import Dict, List, Optional, Set, Tuple, Union
+from functools import cache, cmp_to_key, lru_cache, reduce
+from itertools import combinations, pairwise, permutations, zip_longest
+from math import ceil, factorial, floor, inf, sqrt
+from typing import Deque, Dict, List, Optional, Set, Tuple, Union
 EOF
 
 # Open VSCode with the current directory and focus on the new file at the last line with the cursor at the end

91b8e321981747f9f879a19d13270b990a5d3b60
Kunal Chand <kunalchand234@gmail.com>
2024-06-25 11:52:05 -0400

    python imports

--- p.sh
+++ p.sh
@@ -38,6 +38,7 @@ from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insor
 from collections import Counter, defaultdict, deque
 from dataclasses import dataclass
 from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop
 from itertools import combinations, pairwise, permutations, zip_longest
 from math import ceil, factorial, floor, inf, sqrt
 from typing import Deque, Dict, List, Optional, Set, Tuple, Union

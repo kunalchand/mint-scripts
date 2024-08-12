ba0089efa8760f730beb19cb4b5299e04b5dc96a
Kunal Chand <kunalchand234@gmail.com>
2024-08-12 18:00:37 -0400

    location of my-resource changed

--- editresources.sh
+++ editresources.sh
@@ -3,7 +3,7 @@
 # Set the script to exit immediately if any command fails
 set -e
 
-cd /home/kunalchand/Desktop/Projects/my-resources
+cd /home/kunalchand/Desktop/Projects/Others/my-resources
 
 # Open VSCode in a new session which detaches it fromt the terminal and runs independently
 setsid code . "$@" > /dev/null 2>&1 &

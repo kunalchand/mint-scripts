f0bc2f05e1f004b8b565a7aa3bbe8fa9ee9063b3
Kunal Chand <kunalchand234@gmail.com>
2025-01-07 16:13:43 -0500

    trulogik scripts

---
+++ trulogik-scripts/copy.sh
@@ -0,0 +1,12 @@
+# copy docker-compose.yml
+scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/docker-compose.yml postgresadmin@20.102.88.201:/home/postgresadmin/kafka/test
+
+# copy postgres-sink-connector.json
+scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/postgres-sink-connector.json postgresadmin@20.102.88.201:/home/postgresadmin/kafka/test
+
+# go to kafka-integration and build the project
+cd /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration
+mvn clean package
+
+# copy bundled jars
+scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/target/*.jar postgresadmin@20.102.88.201:/home/postgresadmin/kafka/test

8212be047658f1b7999b5065aedb7f2365c85986
Kunal Chand <kunalchand234@gmail.com>
2025-01-07 17:15:26 -0500

    nobuild and keepopen

--- trulogik-scripts/copy.sh
+++ trulogik-scripts/copy.sh
@@ -4,8 +4,16 @@ scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/docker-
 # copy postgres-sink-connector.json
 scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/postgres-sink-connector.json postgresadmin@20.102.88.201:/home/postgresadmin/kafka/
 
-# Check if the first argument is not "nobuild"
-if [ "$1" == "nobuild" ]; then
+# Check for "nobuild" argument (exact match)
+nobuild_found=false
+for arg in "$@"; do
+  if [[ "$arg" == "nobuild" ]]; then
+    nobuild_found=true
+    break
+  fi
+done
+
+if $nobuild_found; then
     echo "Skipping build process..."
 else
     # go to kafka-integration and build the project
@@ -15,3 +23,20 @@ fi
 
 # copy bundled jars
 scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/target/*.jar postgresadmin@20.102.88.201:/home/postgresadmin/kafka/kafka-connect-jars/
+
+# Check for "keepopen" argument (exact match)
+keepopen_found=false
+for arg in "$@"; do
+  if [[ "$arg" == "keepopen" ]]; then
+    keepopen_found=true
+    break
+  fi
+done
+
+# Close terminal only if "keepopen" is NOT found
+if ! $keepopen_found; then
+    # get the PID of the terminal
+    TERMINAL_PID=$(ps -o ppid= -p $$)
+    # close terminal
+    kill -9 "$TERMINAL_PID"
+fi

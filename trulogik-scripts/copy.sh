1fe3bff61e8730d6b28dbd0c774a65f31b92059f
Kunal Chand <kunalchand234@gmail.com>
2025-01-07 16:29:45 -0500

    copy to correct directory in vm

--- trulogik-scripts/copy.sh
+++ trulogik-scripts/copy.sh
@@ -1,12 +1,12 @@
 # copy docker-compose.yml
-scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/docker-compose.yml postgresadmin@20.102.88.201:/home/postgresadmin/kafka/test
+scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/docker-compose.yml postgresadmin@20.102.88.201:/home/postgresadmin/kafka/
 
 # copy postgres-sink-connector.json
-scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/postgres-sink-connector.json postgresadmin@20.102.88.201:/home/postgresadmin/kafka/test
+scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/postgres-sink-connector.json postgresadmin@20.102.88.201:/home/postgresadmin/kafka/
 
-# go to kafka-integration and build the project
-cd /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration
+# # go to kafka-integration and build the project
+cd /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/
 mvn clean package
 
 # copy bundled jars
-scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/target/*.jar postgresadmin@20.102.88.201:/home/postgresadmin/kafka/test
+scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/target/*.jar postgresadmin@20.102.88.201:/home/postgresadmin/kafka/kafka-connect-jars/

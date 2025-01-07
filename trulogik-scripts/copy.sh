0a16cb68dac7f49deb70b8df99326af962d736dd
Kunal Chand <kunalchand234@gmail.com>
2025-01-07 16:35:27 -0500

    nobuild parameter option

--- trulogik-scripts/copy.sh
+++ trulogik-scripts/copy.sh
@@ -4,9 +4,14 @@ scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/docker-
 # copy postgres-sink-connector.json
 scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/postgres-sink-connector.json postgresadmin@20.102.88.201:/home/postgresadmin/kafka/
 
-# # go to kafka-integration and build the project
-cd /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/
-mvn clean package
+# Check if the first argument is not "nobuild"
+if [ "$1" == "nobuild" ]; then
+    echo "Skipping build process..."
+else
+    # go to kafka-integration and build the project
+    cd /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/
+    mvn clean package
+fi
 
 # copy bundled jars
 scp /home/kunalchand/Desktop/Projects/trulogik-company/kafka-integration/target/*.jar postgresadmin@20.102.88.201:/home/postgresadmin/kafka/kafka-connect-jars/
